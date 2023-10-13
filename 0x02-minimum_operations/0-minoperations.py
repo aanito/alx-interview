bb634c72fc508dc25977967c0f0a01378946b7fb
root <root@DESKTOP-MCL0TE2>
2023-10-13 05:43:05 +0300

    Revised script

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
-"""Given a number n, write a method that calculates
-the fewest number of operations needed to
-result in exactly n H characters in the file."""
+'''The minimum operations coding challenge.
+'''
 
 
 def minOperations(n):
-    """Calculate how many operations is needed to
-    get to n"""
-    num, num_of_operations, cp_paste_operation = 1, 0, 2
-    paste_operation, flag, i, count = 1, 1, 0, 0
-    increment = [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049]
-    if type(n) != int:
-        raise TypeError('Expected Integer as parameter')
-    if n <= 0:
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
         return 0
-    while (num != n and num < n):
-        count += 1
-        if count == 100:
-            return 0
-        if flag == 1 and n - num > increment[i]:
-            if num_of_operations == 0:
-                num_of_operations = cp_paste_operation
-                num = num + increment[i]
-            else:
-                num_of_operations = num_of_operations + cp_paste_operation
-                if n - num > i:
-                    i = i + 1
-                while (increment[i] > n - num):
-                    i = i - 1
-                num = num + increment[i]
-            flag = 0
-        else:
-            while (increment[i] > n - num):
-                num_of_operations += 1
-                i = i - 1
-            num = num + increment[i]
-            num_of_operations = num_of_operations + paste_operation
-            flag = 1
-    return num_of_operations
-
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
